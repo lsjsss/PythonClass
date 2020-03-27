54099b1747b7707cc55b6f44f874fd94eb130038
Mr <lsjsss@live.cn>
2020-03-27 22:10:27 +0800

    3.27 22 - update for 3.27 python class

--- PythonClass-CF/Python_class3_20.py
+++ PythonClass-CF/Python_class3_20.py
@@ -1,4 +1,12 @@
-import math
-print(math.sin(1))
+# import math
+#
+# print(math.sin(1))
 
-
+number1 = [0, 1, 2, 3, 4, 5, 6, 7]
+print(number1[0:3])  # 取下标0~2的字符
+print(number1[2:])  # 取下标从2开始到最后的字符
+print(number1[:2])  # 取下标从0开始到1的字符
+print(number1[:])   # 取字符串所有的值
+print(number1[::2])  # 从头到尾，取步长为2对应的字符
+print(number1[1:-1])  # 取下标从1开始到倒数第二个（下标为 -1）字符
+print(number1[0:6:3])  # 取下标从0~5并且步长为3对应的字符
