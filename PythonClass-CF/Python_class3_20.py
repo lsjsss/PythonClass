1bc0cde8d999ac27f6dfb07fb4ac127b99f59627
Mr <lsjsss@live.cn>
2020-04-09 20:11:56 +0800

    4.9 22 - update for 4.9 python class

--- PythonClass-CF/Python_class3_20.py
+++ PythonClass-CF/Python_class3_20.py
@@ -10,3 +10,10 @@ print(number1[:])   # 取字符串所有的值
 print(number1[::2])  # 从头到尾，取步长为2对应的字符
 print(number1[1:-1])  # 取下标从1开始到倒数第二个（下标为 -1）字符
 print(number1[0:6:3])  # 取下标从0~5并且步长为3对应的字符
+
+# a = tuple(range(1, 16))
+# print(a)
+#
+# b = {'we': 2, 'ppp': 3}
+# type(a)
+# d = dict(e=1, f=2, g=3)
