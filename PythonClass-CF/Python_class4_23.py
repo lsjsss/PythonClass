3b99d8fadef2f787542644f2c1bfcbe7736d02a7
Mr <lsjsss@live.cn>
2020-04-23 18:18:00 +0800

    4.23 18 - update Python book Uint 1-6

--- PythonClass-CF/Python_class4_23.py
+++ PythonClass-CF/Python_class4_23.py
@@ -1,10 +1,10 @@
+# 输出杨辉三角形
 def triangle():
     N = [1]
     while True:
         yield N  # generator特点在于：在执行过程中，遇到yield就中断，下次又继续执行
         N.append(0)  # 每次都要在最后一位加个0，用于后续的叠加
         N = [N[i] + N[i - 1] for i in range(len(N))]
-
 
 def print_triangle(x):
     a = 0
@@ -14,8 +14,9 @@ def print_triangle(x):
         if a == x:
             break
 
-
 print_triangle(10)  # 打印10行
+
+
 
 N = [1]
 for i in range(10):
